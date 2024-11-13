392d60d57853e8508643a2f989fb7a9c8dd38f11
piliran <99893165+piliran@users.noreply.github.com>
2024-11-13 15:53:18 +0200

    push

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,22 +1,199 @@
-<script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
-</script>
-
 <template>
     <AppLayout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
+        <div
+            class="py-8 w-full mt-6 px-6 bg-white shadow-md overflow-hidden sm:rounded-lg"
+        >
+            <Chart
+                type="line"
+                :data="chartData"
+                :options="chartOptions"
+                class="h-[30rem]"
+            />
+        </div>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
-                </div>
-            </div>
+        <div
+            class="py-8 w-full mt-6 px-6 bg-white shadow-md overflow-hidden sm:rounded-lg"
+        >
+            <Chart
+                type="bar"
+                :data="barChartData"
+                :options="barChartOptions"
+                class="h-[30rem]"
+            />
         </div>
     </AppLayout>
 </template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Chart from "primevue/chart";
+
+onMounted(() => {
+    chartData.value = setChartData();
+    chartOptions.value = setChartOptions();
+    barChartData.value = setBarChartData();
+    barChartOptions.value = setBarChartOptions();
+});
+
+const chartData = ref();
+const chartOptions = ref();
+const barChartData = ref();
+const barChartOptions = ref();
+
+const setChartData = () => {
+    const documentStyle = getComputedStyle(document.documentElement);
+
+    return {
+        labels: [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+        ],
+        datasets: [
+            {
+                label: "First Dataset",
+                data: [65, 59, 80, 81, 56, 55, 40],
+                fill: false,
+                tension: 0.4,
+                borderColor: documentStyle.getPropertyValue("--p-cyan-500"),
+            },
+            {
+                label: "Second Dataset",
+                data: [28, 48, 40, 19, 86, 27, 90],
+                fill: false,
+                borderDash: [5, 5],
+                tension: 0.4,
+                borderColor: documentStyle.getPropertyValue("--p-orange-500"),
+            },
+            {
+                label: "Third Dataset",
+                data: [12, 51, 62, 33, 21, 62, 45],
+                fill: true,
+                borderColor: documentStyle.getPropertyValue("--p-gray-500"),
+                tension: 0.4,
+                backgroundColor: "rgba(107, 114, 128, 0.2)",
+            },
+        ],
+    };
+};
+const setChartOptions = () => {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue("--p-text-color");
+    const textColorSecondary = documentStyle.getPropertyValue(
+        "--p-text-muted-color"
+    );
+    const surfaceBorder = documentStyle.getPropertyValue(
+        "--p-content-border-color"
+    );
+
+    return {
+        maintainAspectRatio: false,
+        aspectRatio: 0.6,
+        plugins: {
+            legend: {
+                labels: {
+                    color: textColor,
+                },
+            },
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: textColorSecondary,
+                },
+                grid: {
+                    color: surfaceBorder,
+                },
+            },
+            y: {
+                ticks: {
+                    color: textColorSecondary,
+                },
+                grid: {
+                    color: surfaceBorder,
+                },
+            },
+        },
+    };
+};
+
+const setBarChartData = () => {
+    const documentStyle = getComputedStyle(document.documentElement);
+
+    return {
+        labels: [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+        ],
+        datasets: [
+            {
+                label: "My First dataset",
+                backgroundColor: documentStyle.getPropertyValue("--p-cyan-500"),
+                borderColor: documentStyle.getPropertyValue("--p-cyan-500"),
+                data: [65, 59, 80, 81, 56, 55, 40],
+            },
+            {
+                label: "My Second dataset",
+                backgroundColor: documentStyle.getPropertyValue("--p-gray-500"),
+                borderColor: documentStyle.getPropertyValue("--p-gray-500"),
+                data: [28, 48, 40, 19, 86, 27, 90],
+            },
+        ],
+    };
+};
+const setBarChartOptions = () => {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue("--p-text-color");
+    const textColorSecondary = documentStyle.getPropertyValue(
+        "--p-text-muted-color"
+    );
+    const surfaceBorder = documentStyle.getPropertyValue(
+        "--p-content-border-color"
+    );
+
+    return {
+        maintainAspectRatio: false,
+        aspectRatio: 0.8,
+        plugins: {
+            legend: {
+                labels: {
+                    color: textColor,
+                },
+            },
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: textColorSecondary,
+                    font: {
+                        weight: 500,
+                    },
+                },
+                grid: {
+                    display: false,
+                    drawBorder: false,
+                },
+            },
+            y: {
+                ticks: {
+                    color: textColorSecondary,
+                },
+                grid: {
+                    color: surfaceBorder,
+                    drawBorder: false,
+                },
+            },
+        },
+    };
+};
+</script>
