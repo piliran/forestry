c06f6c1c944321d64a89e21136c115e5b91f09ca
WONGANM <31410583+WONGANM@users.noreply.github.com>
2024-11-27 20:12:19 +0200

    push

--- database/migrations/2024_11_16_051342_create_operations_table.php
+++ database/migrations/2024_11_16_051342_create_operations_table.php
@@ -24,8 +24,8 @@ return new class extends Migration
             $table->string('funded_by');
             $table->unsignedBigInteger('route_id');
             $table->foreign('route_id')->references('id')->on('routes')->onDelete('cascade')->onUpdate('cascade');
-            $table->unsignedBigInteger('team_leader');
-            $table->foreign('team_leader')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedBigInteger('team_id');
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade')->onUpdate('cascade');
             $table->unsignedBigInteger('operation_type_id');
             $table->foreign('operation_type_id')->references('id')->on('operation_types')->onDelete('cascade')->onUpdate('cascade');
         
